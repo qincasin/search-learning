a4e41adf1864a1b107ccaaac7940ec212a7ed358
秦加兴 <qincasin@163.com>
2022-09-04 15:01:03 +0800

    init

---
+++ lucene/lucene-2/src/main/java/com/qjx/lucene/Lucene2Application.java
@@ -0,0 +1,13 @@
+package com.qjx.lucene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lucene2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lucene2Application.class, args);
+    }
+
+}
---
+++ lucene/lucene-2/src/test/java/com/qjx/lucene/Lucene1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qjx.lucene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lucene1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
